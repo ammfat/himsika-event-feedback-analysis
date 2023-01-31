a686118d845ba2e14c75aca70405600251f26f7d
ammfat <ahmad.mmfat@gmail.com>
2023-01-31 16:07:22 +0700

    Optimize Airflow environment to fit system resources

--- dags/gsheet_to_bq_raw.py
+++ dags/gsheet_to_bq_raw.py
@@ -2,19 +2,15 @@ import os
 import google.auth
 import json
 
-from airflow import DAG, macros
-from airflow.contrib.operators.bigquery_operator import BigQueryOperator
-from airflow.utils.dates import days_ago
+from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
-from googleapiclient.discovery import build
 
 # SETUP CREDENTIALS
 
 scopes = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/cloud-platform']
 service_account_file_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
 CREDENTIALS, PROJECT_ID = google.auth.default(scopes=scopes)
-gdrive_service = build('drive', 'v3', credentials=CREDENTIALS)
 
 # SETUP DAG
 
@@ -23,21 +19,27 @@ args = {
 }
 
 bq_raw_data_dataset = 'event_feedback'
-EXEC_DATE = '{{ ds }}'
-EXEC_DATE_WEEK_AGO = '{{ macros.ds_add(ds, -7) }}'
 
 # DAG
 
-def get_dag_details(**kwargs):
+def _get_dag_details(**kwargs):
     return {
         'project_id': PROJECT_ID
         , 'bq_raw_data_dataset': bq_raw_data_dataset
-        , 'execution_date': kwargs.get('ds')
+        , 'exec_timestamp': kwargs.get('ts')
     }
 
-def gsheet_sensor(start_date, end_date):
-    print(f'gsheet_sensor: start_date={start_date}, end_date={end_date}')
-    print(f'gsheet_sensor: start_date={type(start_date)}, end_date={type(end_date)}')
+def _gsheet_sensor(**kwargs):
+    from googleapiclient.discovery import build
+
+    gdrive_service = build('drive', 'v3', credentials=CREDENTIALS)
+
+    search_start_date = datetime.strptime(kwargs.get('ts'), '%Y-%m-%dT%H:%M:%S%z') - timedelta(days=7)
+    search_end_date = datetime.strptime(kwargs.get('ts'), '%Y-%m-%dT%H:%M:%S%z')
+    search_start_date = search_start_date.strftime('%Y-%m-%dT%H:%M:%S')
+    search_end_date = search_end_date.strftime('%Y-%m-%dT%H:%M:%S')
+
+    print(f"gsheet_sensor: { search_start_date } - { search_end_date }")
 
     tmp_sheets = []
     pageToken = ""
@@ -47,8 +49,8 @@ def gsheet_sensor(start_date, end_date):
             q=f"""
                 mimeType='application/vnd.google-apps.spreadsheet'
                 and not name contains 'output'
-                and createdTime >= '{ start_date }'
-                and createdTime <= '{ end_date }'
+                and createdTime >= '{ search_start_date }'
+                and createdTime <= '{ search_end_date }'
             """
             , fields="nextPageToken, files(id, name, owners, createdTime, modifiedTime)"
         ).execute()
@@ -62,12 +64,15 @@ def gsheet_sensor(start_date, end_date):
     if sheets:
         for sheet in sheets:
             print(sheet['id'].ljust(45), sheet['createdTime'].ljust(25), sheet['name'])
+        
+        return True
     else:
         print('No sheets found.')
+        return False
 
-    return True
+    # return True
 
-def gsheet_to_bq_raw_data():
+def _gsheet_to_bq_raw_data():
     return True
 
 
@@ -75,29 +80,23 @@ with DAG(
         dag_id='gsheet_to_bq_raw'
         , default_args=args
         , schedule_interval='0 3 * * 1'
-        , start_date=datetime(2022, 7, 1)
-        , end_date=datetime(2022, 8, 1)
-        , concurrency=2
-        , max_active_runs=2
+        , start_date=datetime(2022, 6, 1)
+        , end_date=datetime(2022, 11, 1)
     ) as dag:
 
-    print_dag_details = PythonOperator(
-        task_id='print_dag_details'
-        , python_callable=get_dag_details
+    get_dag_details= PythonOperator(
+        task_id='get_dag_details'
+        , python_callable=_get_dag_details
     )
 
     gsheet_sensor = PythonOperator(
         task_id='gsheet_sensor'
-        , python_callable=gsheet_sensor
-        , op_kwargs={
-            'start_date': EXEC_DATE_WEEK_AGO
-            , 'end_date': EXEC_DATE
-        }
+        , python_callable=_gsheet_sensor
     )
 
     gsheet_to_bq_raw_data = PythonOperator(
         task_id='gsheet_to_bq_raw_data'
-        , python_callable=gsheet_to_bq_raw_data
+        , python_callable=_gsheet_to_bq_raw_data
     )
 
     print_dag_details >> gsheet_sensor >> gsheet_to_bq_raw_data

c5c9452ed18a5de7422fcfb47edc30ac38baafd8
ammfat <ahmad.mmfat@gmail.com>
2023-03-07 21:42:53 +0700

    Remove unrequired imports

--- dags/bq_dwh_analytics.py
+++ dags/bq_dwh_analytics.py
@@ -3,11 +3,9 @@ import os
 
 from airflow import DAG
 from airflow.contrib.operators.bigquery_operator import BigQueryOperator
-from airflow.exceptions import AirflowSkipException
-from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator, ShortCircuitOperator
-from airflow.sensors.filesystem import FileSensor
-from datetime import datetime, timedelta
+from airflow.operators.python import PythonOperator
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
+from transformation.sql import Query
 
 # SETUP CREDENTIALS
 
@@ -21,10 +19,16 @@ args = {
     'owner': 'ammfat',
 }
 
-bq_raw_data_dataset = 'raw_data_experiment'
-bq_event_feedback_dataset = 'event_feedback_experiment'
-bq_dwh_event_dataset = 'dwh_event_experiment'
+bq_raw_data_dataset = 'raw_data'
+bq_event_feedback_dataset = 'event_feedback'
+bq_dwh_event_dataset = 'dwh_event'
 
+query = Query(
+    project_id=PROJECT_ID
+    , raw_data_dataset=bq_raw_data_dataset
+    , event_feedback_dataset=bq_event_feedback_dataset
+    , dwh_event_dataset=bq_dwh_event_dataset
+)
 
 with DAG(
         dag_id='bq_dwh_analytics'
@@ -35,21 +39,92 @@ with DAG(
         , description='Load data warehouse tables in BigQuery for analytics'
     ) as dag:
 
-    get_success_signal = FileSensor(
+    get_success_signal = ExternalTaskSensor(
         task_id='get_success_signal'
-        , filepath='/signal/{{ ds_nodash }}/_SUCCESS'
-        , poke_interval=60
-        , timeout=60*30
+        , poke_interval=10
+        , retries=2
+        # , timeout=60
+        # , mode='reschedule'
+        , soft_fail=True
+        , external_dag_id='gsheet_to_bq'
+        , external_task_id='signal_to_dwh_dag'
     )
 
-    task_resume_decision_maker = ShortCircuitOperator(
-        task_id='task_resume_decision_maker'
-        , python_callable=lambda: False
+    create_bq_dim_events_history = BigQueryOperator(
+        task_id='create_bq_dim_events_history'
+        , sql=query.get_sql_dim_events_history()
+        , use_legacy_sql=False
     )
 
-    get_status = BashOperator(
-        task_id='get_status'
-        , bash_command='echo "File exists"'
+    create_bq_dim_instances_history = BigQueryOperator(
+        task_id='create_bq_dim_instances_history'
+        , sql=query.get_sql_dim_instances_history()
+        , use_legacy_sql=False
     )
 
-    get_success_signal >> task_resume_decision_maker >> get_status
+    create_bq_dim_degree_programs_history = BigQueryOperator(
+        task_id='create_bq_dim_degree_programs_history'
+        , sql=query.get_sql_dim_degree_programs_history()
+        , use_legacy_sql=False
+    )
+
+    create_bq_professions_history = BigQueryOperator(
+        task_id='create_bq_professions_history'
+        , sql=query.get_sql_professions_history()
+        , use_legacy_sql=False
+    )
+
+    create_bq_dim_events = BigQueryOperator(
+        task_id='create_bq_dim_events'
+        , sql=query.get_sql_dim_events()
+        , use_legacy_sql=False
+    )
+
+    create_bq_dim_instances = BigQueryOperator(
+        task_id='create_bq_dim_instances'
+        , sql=query.get_sql_dim_instances()
+        , use_legacy_sql=False
+    )
+
+    create_bq_dim_degree_programs = BigQueryOperator(
+        task_id='create_bq_dim_degree_programs'
+        , sql=query.get_sql_dim_degree_programs()
+        , use_legacy_sql=False
+    )
+
+    create_bq_dim_professions = BigQueryOperator(
+        task_id='create_bq_dim_professions'
+        , sql=query.get_sql_dim_professions()
+        , use_legacy_sql=False
+    )
+
+    create_fact_rates_by_responses = BigQueryOperator(
+        task_id='create_fact_rates_by_responses'
+        , sql=query.get_sql_fact_rates_by_responses()
+        , use_legacy_sql=False
+    )
+
+    create_sql_view_rates_by_responses = BigQueryOperator(
+        task_id='create_sql_view_rates_by_responses'
+        , sql=query.get_sql_view_rates_by_responses()
+        , use_legacy_sql=False
+    )
+
+    get_success_signal >> [
+        create_bq_dim_events_history
+        , create_bq_dim_instances_history
+        , create_bq_dim_degree_programs_history
+        , create_bq_professions_history
+    ]
+
+    create_bq_dim_events_history >> create_bq_dim_events
+    create_bq_dim_instances_history >> create_bq_dim_instances
+    create_bq_dim_degree_programs_history >> create_bq_dim_degree_programs
+    create_bq_professions_history >> create_bq_dim_professions
+
+    [
+        create_bq_dim_events
+        , create_bq_dim_instances
+        , create_bq_dim_degree_programs
+        , create_bq_dim_professions
+    ] >> create_fact_rates_by_responses >> create_sql_view_rates_by_responses
